5c2ec6f75d3a4848d79016b156e394932da1f0fa
Matin Kaviani <59339049+matinkaviani@users.noreply.github.com>
2023-12-28 13:28:50 +0330

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,60 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+"use client"
+import "react-light-table/dist/style.css";
+import ReactLightTable from "../../../dist/esm/components/ReactLightTable";
 export default function Home() {
+  const columns = [
+    {
+      key: "are",
+      title: "are",
+      sortable: true
+    },
+    {
+      key: "na",
+      title: "na",
+      sortable: false,
+      headClassName: "dsdsd"
+    },
+    {
+      key: "shayad",
+      title: "shayad",
+      sortable: true,
+      headClassName: "dsdsd",
+    },
+    {
+      key: "baba",
+      title: "baba",
+      sortable: true
+    }
+  ];
+  const data = [
+    {
+      "are": "matin",
+      "na": "kaviani",
+      "shayad": "12",
+      "baba": "test"
+    },
+    {
+      "are": "reza",
+      "na": "darani",
+      "shayad": "8",
+      "baba": "tset"
+    },
+    {
+      "are": "John",
+      "na": "Smith",
+      "shayad": "1000",
+      "baba": "lorem"
+    },
+  ]
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <ReactLightTable
+        columns={columns}
+        data={data}
+        numberRows
+        initSort={{ key: "shayad", mode: "desc" }}
+      />
+    </div>
   )
 }
