352a4300840f0ad8c7687ea4a84d8b152570fe46
Matin Kaviani <59339049+matinkaviani@users.noreply.github.com>
2024-01-25 14:49:22 +0330

    Deploy Next.js app to GitHub Pages

---
+++ server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"rDjiXzAIBzfgEUc2XssPyswtBp4EpI1diQOKxLZG0MI=\"}"
---
+++ static/chunks/app/layout-c7e76a0236834d5f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7080:function(e,n,_){Promise.resolve().then(_.t.bind(_,3649,23))},3649:function(e){e.exports={style:{fontFamily:"'__Inter_e66fe9', '__Inter_Fallback_e66fe9'",fontStyle:"normal"},className:"__className_e66fe9"}}},function(e){e.O(0,[971,938,744],function(){return e(e.s=7080)}),_N_E=e.O()}]);
